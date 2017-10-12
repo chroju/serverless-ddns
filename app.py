47eb65a73218031af528c3d9e1a0b6b9043e80ab
chroju <chor.chroju@gmail.com>
2017-10-12 09:01:52 +0900

    :construction: WIP PUT method

--- app.py
+++ app.py
@@ -17,16 +17,44 @@ def index():
         return json.dumps({}, indent=2, separators=SEPS)
 
 
-@app.route('/{hostzone}/{name}')
+@app.route('/{hostzone}/{name}', methods=['GET', 'PUT'])
 def state_of_record(hostzone, name):
-    record_sets = _get_dns_record(hostzone=hostzone, name=name)
+    request = app.current_request
 
-    if len(record_sets):
-        record_set_detail = [{'value': i['Value'], 'type': i['Type'], 'ttl': i['TTL']} for i in record_sets['ResourceRecords']]
-        result_json = { 'status': 'exist', 'detail': record_set_detail }
+    if request.method == 'GET':
+        record_sets = _get_dns_record(hostzone=hostzone, name=name)
+        if len(record_sets):
+            record_set_detail = [{'value': i['Value'], 'type': i['Type'], 'ttl': i['TTL']} for i in record_sets['ResourceRecords']]
+            result_json = { 'status': 'exist', 'detail': record_set_detail }
+            return json.dumps(result_json, indent=2, separators=SEPS)
+        else:
+            return json.dumps({"status": "absent"}, indent=2, separators=SEPS)
+    elif request.method == 'PUT':
+        if request.json_body:
+            records = request.json_body['records']
+            record_type = request.json_body['type']
+            ttl = request.json_body['ttl']
+        else:
+            records = [request.context['identity']['sourceIp']]
+            record_type = 'A'
+            ttl = 300
+
+        result = client.change_resource_records_sets(
+            HostedZoneId=hostzone,
+            ChangeBatch={
+                Changes:[
+                    {
+                        'Action': action,
+                        'ResourceRecordSet': {
+                            'Name'
+                        }
+                    }
+                ]
+            }
+        )['ChangeInfo']
+
+        result_json = { 'status': result['Status'], detail}
         return json.dumps(result_json, indent=2, separators=SEPS)
-    else:
-        return json.dumps({"status": "absent"}, indent=2, separators=SEPS)
 
 
 def _get_dns_hosted_zones():
